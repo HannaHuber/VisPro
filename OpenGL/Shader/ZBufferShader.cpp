df27ca4a48d5da149f3151d7d753ce301dc35ab0
HannaHuber <hanna_huber@hotmail.com>
2016-04-28 10:15:07 +0200

    rm ZBuffer-Geometryshader

--- OpenGL/Shader/ZBufferShader.cpp
+++ OpenGL/Shader/ZBufferShader.cpp
@@ -1,7 +1,7 @@
 #include "ZBufferShader.h"
 
 ZBufferShader::ZBufferShader() 
-: Shader("./Shader/ZBufferShader.vert", "./Shader/ZBufferShader.frag", "./Shader/ZBufferShader.geom")
+: Shader("./Shader/ZBufferShader.vert", "./Shader/ZBufferShader.frag")
 {
 
 }
